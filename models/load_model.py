55fccdc05904524279e6061a75cbb894392ca3ee
Enes Demirag <ensdmrg@gmail.com>
2020-08-14 19:52:25 +0300

    Remove extra stuff

--- models/load_model.py
+++
@@ -1,4 +0,0 @@
-from gcnn.network import *
-from edgeconnect.network import InpaintGenerator, EdgeGenerator, Discriminator
-from ...scripts.loss import AdversarialLoss, PerceptualLoss, StyleLoss
-from ...scripts.config import Config
