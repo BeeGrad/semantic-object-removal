Fix masking problems for edgeconnect

scripts/dataOperations.py
@@ -7,6 +7,9 @@ import cv2
 import os, os.path
 from skimage.feature import canny
 from skimage.color import rgb2gray
+from scripts.config import Config
+
+cfg = Config()
 
 class DataRead():
     def __init__(self, dataset="cifar10", masking_type="lines", batch_size = 10):
@@ -76,6 +79,7 @@ class DataRead():
 
             for filename in os.listdir(self.path):
                 img = cv2.imread(os.path.join(self.path,filename))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 if img is not None:
                     imgs.append(img)
                     # print(f"Image {filename} is loaded")
@@ -115,7 +119,7 @@ class DataRead():
                 sample = random.randint(0,self.data.shape[0]-1)
                 img = self.data[sample]
                 fig.add_subplot(rows, columns, i)
-                plt.imshow(img.permute(1,2,0).numpy().astype(int))
+                plt.imshow(img.permute(1,2,0).numpy())
             plt.show()
 
     def show_masked_and_original(self):
@@ -131,11 +135,19 @@ class DataRead():
             sample = random.randint(0,self.data.shape[0]-1)
             im = self.data[sample]
             masked_im = self.masked_data[sample]
+            mask = self.masks[sample]
+            edge = self.edges[sample]
+            mask = mask.squeeze()
+            edge = edge.squeeze()
             fig=plt.figure(figsize=(1, 2))
-            fig.add_subplot(1, 2, 1)
-            plt.imshow(im.permute(1,2,0).numpy().astype(int))
-            fig.add_subplot(1, 2, 2)
-            plt.imshow(masked_im.permute(1,2,0).numpy().astype(int))
+            fig.add_subplot(2, 2, 1)
+            plt.imshow(im.permute(1,2,0).numpy())
+            fig.add_subplot(2, 2, 2)
+            plt.imshow(masked_im.permute(1,2,0).numpy())
+            fig.add_subplot(2, 2, 3)
+            plt.imshow(mask.numpy(), cmap='gray')
+            fig.add_subplot(2, 2, 4)
+            plt.imshow(edge.numpy(), cmap='gray')
             plt.show()
 
     def create_masked_data(self):
@@ -170,17 +182,17 @@ class DataRead():
                     # Get random thickness of the line drawn
                     thickness = np.random.randint(1, 3)
                     # Draw black line on the white mask
-                    cv2.line(mask,(x1,y1),(x2,y2),(0,0,0),thickness)
+                    cv2.line(mask,(x1,y1),(x2,y2),(255,255,255),thickness)
 
                 ## Mask the image
                 masked_image = self.data[img].copy()
-                masked_image[mask == 0] = 255
+                masked_image[mask == 255] = 255
                 mask = mask[
                     :, :, 0
                 ]  # Mask should be 2 dimensional for the rest of the operations
                 self.masks[img] = mask
-                self.gray_data[img] = rgb2gray(self.data[img])
-                self.edges[img] = canny(self.gray_data[img], sigma=2, mask=mask)
+                self.gray_data[img] = cv2.cvtColor(self.data[img], cv2.COLOR_RGB2GRAY)
+                self.edges[img] = cv2.Canny(self.gray_data[img], cfg.thresh1, cfg.thresh2)
                 self.masked_data[img] = masked_image
 
         if self.masking_type == '10-20percentage':
@@ -196,18 +208,19 @@ class DataRead():
                 start_point = (start_x, start_y)
                 end_point = (start_x + off_x, start_y + off_y)
 
-                mask = np.full((image_width,image_heigth, image_channel), 255, np.uint8) ## White background
-                cv2.rectangle(mask, start_point, end_point, (0,0,0), -1)
+                mask = np.full((image_width,image_heigth, image_channel), 0, np.uint8) ## White background
+                cv2.rectangle(mask, start_point, end_point, (255,255,255), -1)
 
                 ## Mask the image
                 masked_image = self.data[img].copy()
-                masked_image[mask == 0] = 255
+                masked_image[mask == 255] = 255
                 mask = mask[
                     :, :, 0
                 ]  # Mask should be 2 dimensional for the rest of the operations
                 self.masks[img] = mask
-                self.gray_data[img] = rgb2gray(self.data[img])
-                self.edges[img] = canny(self.gray_data[img], sigma=2, mask=mask)
+                self.gray_data[img] = cv2.cvtColor(self.data[img], cv2.COLOR_RGB2GRAY)
+                self.edges[img] = cv2.Canny(self.gray_data[img], cfg.thresh1, cfg.thresh2)
+                self.edges[img] = np.abs(self.edges[img] - 255)
                 self.masked_data[img] = masked_image
 
 
@@ -225,11 +238,11 @@ class DataRead():
         self.get_data()
         self.create_masked_data()
 
-        self.data = torch.FloatTensor(self.data)
-        self.masked_data = torch.FloatTensor(self.masked_data)
-        self.gray_data = torch.FloatTensor(self.gray_data)
-        self.masks = torch.FloatTensor(self.masks)
-        self.edges = torch.FloatTensor(self.edges)
+        self.data = torch.FloatTensor(self.data) / 255
+        self.masked_data = torch.FloatTensor(self.masked_data) / 255
+        self.gray_data = torch.FloatTensor(self.gray_data) / 255
+        self.masks = torch.FloatTensor(self.masks) / 255
+        self.edges = torch.FloatTensor(self.edges) / 255
 
         self.gray_data = self.gray_data.unsqueeze(1)
         self.edges = self.edges.unsqueeze(1)
@@ -245,6 +258,51 @@ class DataRead():
 
         dataset = torch.utils.data.TensorDataset(
             self.data, self.masked_data, self.gray_data, self.masks, self.edges
+        )
+        self.train_data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=self.batch_size
+        )
+
+        self.data = torch.FloatTensor(self.data)
+        self.masked_data = torch.FloatTensor(self.masked_data)
+        dataset = torch.utils.data.TensorDataset(self.masked_data) ## Buraya daha sonra bir bak hata var gibi
+        self.test_data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=self.batch_size
+        )
+
+    def create_data_loaders_edgeconnect(self):
+        """
+        Input:
+            none
+        Output:
+            none
+        Description:
+            Creates necessary data laoders for pytorch with specified batch size.
+        """
+
+        self.get_data()
+        self.create_masked_data()
+
+        self.data = torch.FloatTensor(self.data) / 255
+        self.masked_data = torch.FloatTensor(self.masked_data) / 255
+        self.gray_data = torch.FloatTensor(self.gray_data) / 255
+        self.masks = torch.FloatTensor(self.masks) / 255
+        self.edges = torch.FloatTensor(self.edges) / 255
+
+        self.gray_data = self.gray_data.unsqueeze(1)
+        self.edges = self.edges.unsqueeze(1)
+        self.masks = self.masks.unsqueeze(1)
+        self.data = self.data.permute(0, 3, 1, 2)
+        self.masked_data = self.masked_data.permute(0, 3, 1, 2)
+
+        print(f"Masks shape: {self.masks.shape}")
+        print(f"Edges shape: {self.edges.shape}")
+        print(f"Gray_data shape: {self.gray_data.shape}")
+        print(f"masked_data shape: {self.masked_data.shape}")
+        print(f"data shape: {self.data.shape}")
+
+        dataset = torch.utils.data.TensorDataset(
+            self.data, self.gray_data, self.masks, self.edges
         )
         self.train_data_loader = torch.utils.data.DataLoader(
             dataset, batch_size=self.batch_size

utils/utils.py
@@ -253,18 +253,16 @@ def local_patch(x, bbox_list):
     return torch.stack(patches, dim=0)
 
 def spatial_discounting_mask():
-    """Generate spatial discounting mask constant.
-
-    Spatial discounting mask is first introduced in publication:
-        Generative Image Inpainting with Contextual Attention, Yu et al.
-
-    Args:
+    """
+    Input:
         config: Config should have configuration including HEIGHT, WIDTH,
-            DISCOUNTED_MASK.
-
-    Returns:
+        DISCOUNTED_MASK.
+    Output:
         tf.Tensor: spatial discounting mask
-
+    Description:
+        Generate spatial discounting mask constant.
+        Spatial discounting mask is first introduced in publication:
+            Generative Image Inpainting with Contextual Attention, Yu et al.
     """
     gamma = cfg.spatial_discounting_mask
     height, width = cfg.context_mask_shape